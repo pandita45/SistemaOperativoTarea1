374f399b3923d476bc0a3f63177c84e83cde7800
pandita45 <jacastillo2024@udec.cl>
2025-09-13 00:04:51 -0300

    Agregacion del comando personalizado miprof

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 int manejarInput(char* cadena){
     for(int i = 0; i < strlen(cadena); i++) if(cadena[i] == '\n') {cadena[i] = '\0'; break;}
     if (strcmp(cadena,"") == 0) return 0;
-    if (strcmp(cadena,"salir") == 0) return -1;
+    if (strcmp(cadena,"exit") == 0) return -1;
     
     int it, pipes;
     for (it = 0, pipes = 0; cadena[it]; it++)
@@ -38,7 +38,7 @@ int manejarInput(char* cadena){
         int argc;
         for(argc = 0; token != NULL && argc < MAXARGSIZE; argc++){
             argv[i][argc] = token;
-            token = strtok(NULL, " ");
+            token = strtok(NULL, " "); // siguiente token
         }
         argv[i][argc] = NULL;
     }
@@ -49,6 +49,55 @@ int manejarInput(char* cadena){
     pid_t child_pids[pipes+1];
 
     for(int i = 0; i <= pipes; i++){
+
+        if (strcmp(argv[i][0], "help") == 0) {
+            printf("  exit   - Cierra la shell\n");
+            printf("  help   - Muestra esta ayuda\n");
+            continue; 
+        }
+        if(strcmp(argv[i][0], "miprof") == 0){
+            if(strcmp(argv[i][1], "ejec") == 0  || argv[i][1] == NULL){
+                if(argv[i][2] == NULL){
+                    fprintf(stderr, "Error: Use 'miprof help', para mas informacion.\n");
+                    return 0;
+                }
+                else{
+                    // eliminar "miprof" y "ejec" y que quede solo el comando a ejecutar 
+                    int j;  
+                    for(j = 0; argv[i][j + 2] != NULL; j++){
+                        argv[i][j] = argv[i][j + 2];
+                    }
+                    argv[i][j] = NULL; 
+                    struct timeval start, end;
+                    gettimeofday(&start, NULL);
+                    
+                    pid_t pid = fork();
+                    if (pid == 0) {
+                        execvp(argv[i][0], argv[i]);
+                        fprintf(stderr, "Comando no encontrado: %s \n Pruebe usando 'help'\n", argv[i][0]);
+                        exit(1);
+                    }
+                    int status;
+                    waitpid(pid, &status, 0);
+                    gettimeofday(&end, NULL);
+                    double tiempoReal = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+                    printf("Tiempo real: %.6f segundos\n", tiempoReal);
+                    continue;
+                }
+
+            } 
+            else if(strcmp(argv[i][1], "help") == 0){
+                printf("miprof ejec <comando>\n");
+                printf("Ej: miprof ejec ls -l\n");
+                continue;
+            }
+            else {
+                fprintf(stderr, "Error: Use 'miprof help', para mas informacion.\n");
+                return 0;
+            }
+        }
+
+    
         if(i < pipes) {
             pipe(fd[i]);
         }
@@ -60,12 +109,12 @@ int manejarInput(char* cadena){
             if (i < pipes) {
                 dup2(fd[i][1], 1);
             }
-            for(int j = 0; j < pipes; j++){
+            for(int j = 0; j < pipes; j++){ 
                 close(fd[j][0]);
                 close(fd[j][1]);
             }
-            execvp(argv[i][0], argv[i]);
-            perror("execvp failed");
+            execvp(argv[i][0], argv[i]); 
+            fprintf(stderr, "Comando no encontrado: %s \n Pruebe usando 'help'\n", argv[i][0]);
             exit(1);
 
         }
@@ -92,4 +141,6 @@ int main(int argc, const char **argv) {
         printf(PROMPT);
         fgets(i, BUFSIZ, stdin);
     }
+    printf("Saliendo...\n");
+    return 0;
 }
