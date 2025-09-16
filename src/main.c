8a06ce6d8431f80f357207867909bf8b95ff4935
mihn <martin.henrnav@gmail.com>
2025-09-16 00:10:12 -0300

    Crear comando ejecsave

--- src/main.c
+++ src/main.c
@@ -104,17 +104,62 @@ int manejarInput(char* cadena){
                 printf("miprof ejec <comando>\n");
                 printf("Ej: miprof ejec ls -l\n");
                 continue;
-            }
-            else {
+            } else if (strcmp(argv[i][1], "ejecsave") == 0) {
+                if (argv[i][2] == NULL || argv[i][3] == NULL) { // necesita al menos un comando y un archivo
+                    fprintf(stderr, "Error: Use 'miprof help', para mas informacion.\n");
+                    return 0;
+                } else {
+                    // eliminar "miprof" y "ejecsave" y que quede solo el comando a ejecutar 
+                    int j;  
+                    for (j = 0; argv[i][j + 2] != NULL; j++) {
+                        argv[i][j] = argv[i][j + 2]; // desplazamos todo a la izquierda
+                    }
+
+                    argv[i][j] = NULL; // terminador NULL para execvp
+                    struct timeval start, end; // Variables para medir el tiempo
+                    struct rusage usage; // Variable para obtener el uso de recursos
+
+                    gettimeofday(&start, NULL); // Inicia el tiempo
+                    
+                    pid_t pid = fork(); // Crear un proceso hijo
+                    if (pid == 0) { // Proceso hijo
+                        execvp(argv[i][0], argv[i]); // Ejecuta el comando
+                        fprintf(stderr, "Comando no encontrado: %s \n Pruebe usando 'help'\n", argv[i][0]); // Si exec falla
+                        exit(1); // Salir del proceso hijo
+                    }
+
+                    int status;
+                    waitpid(pid, &status, 0);
+                    //termina el tiempo de uso del proceso hijo
+                    gettimeofday(&end, NULL);
+                    //Obtener el uso de recursos del proceso hijo
+                    getrusage(RUSAGE_CHILDREN, &usage);
+
+                    double tiempoSistema = usage.ru_stime.tv_sec + usage.ru_stime.tv_usec / 1000000.0;
+                    double tiempoUsuario = usage.ru_utime.tv_sec + usage.ru_utime.tv_usec / 1000000.0;
+                    double tiempoReal = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+
+                    FILE *file = fopen(argv[i][j-1], "a"); // Abrir el archivo en modo append
+                    if (file == NULL) {
+                        fprintf(stderr, "Error al abrir el archivo %s para escribir.\n", argv[i][j-1]);
+                        return 1;
+                    }
+
+                    fprintf(file, "Tiempo de usuario: %.6f segundos\n", tiempoUsuario); // Guardar en el archivo
+                    fprintf(file, "Tiempo de sistema: %.6f segundos\n", tiempoSistema); // Guardar en el archivo
+                    fprintf(file, "Tiempo real: %.6f segundos\n", tiempoReal); // Guardar en el archivo
+                    fclose(file); // Cerrar el archivo
+                    continue;
+                }
+            } else {
                 fprintf(stderr, "Error: Use 'miprof help', para mas informacion.\n");
                 return 0;
             }
         }
-
     
-        if(i < pipes) {
+        if(i < pipes)
             pipe(fd[i]);
-        }
+
         child_pids[i] = fork();
         if (child_pids[i] == 0) {
             if (i > 0) {
@@ -130,7 +175,6 @@ int manejarInput(char* cadena){
             execvp(argv[i][0], argv[i]); 
             fprintf(stderr, "Comando no encontrado: %s \n Pruebe usando 'help'\n", argv[i][0]);
             exit(1);
-
         }
     }
 
@@ -139,22 +183,24 @@ int manejarInput(char* cadena){
         close(fd[j][1]);
     }
 
-    for(int i = 0; i <= pipes; i++){
+    for(int i = 0; i <= pipes; i++)
         waitpid(child_pids[i], NULL, 0);
-    }
-    for(int i = 0; i <= pipes; i++){
+
+    for(int i = 0; i <= pipes; i++)
         free(tempPipes[i]);
-    }
 }
 
 int main(int argc, const char **argv) {
     char i[BUFFER_SIZE];
+
     printf(PROMPT);
     fgets(i, BUFSIZ, stdin);
+
     while (manejarInput(i) != -1) {
         printf(PROMPT);
         fgets(i, BUFSIZ, stdin);
     }
+    
     printf("Saliendo...\n");
     return 0;
 }
